4f56f18436b42d30fe6b129a789d1196740b72a6
Hwirin-Kim <kimhuirin@gmail.com>
2023-10-16 20:54:34 +0900

    test : socket io room개설 및 메시지 전송 확인 완료

--- src/pages/Test.tsx
+++ src/pages/Test.tsx
@@ -1,24 +1,79 @@
 import React, { useEffect, useRef, useState } from "react";
+import { io } from "socket.io-client";
 import { socket } from "../service/server";
 
 export default function Test() {
+  const [isConnected, setIsConnected] = useState(socket.connected);
   const [chat, setChat] = useState("");
+  const [room, setRoom] = useState(0);
+  const [receiveChat, setReceiveChat] = useState<string[]>([]);
 
+  useEffect(() => {
+    function onConnect() {
+      setIsConnected(true);
+    }
+
+    function onDisconnect() {
+      setIsConnected(false);
+    }
+
+    socket.on("connect", onConnect);
+    socket.on("disconnect", onDisconnect);
+    socket.on("receive_message_from_room", (message) => {
+      setReceiveChat((prev) => [...prev, message]);
+    });
+
+    return () => {
+      socket.off("connect", onConnect);
+      socket.off("disconnect", onDisconnect);
+      socket.off("receive_message_from_room");
+    };
+  }, []);
   const sendMessage = () => {
-    socket.emit("send_message", chat);
+    socket.emit("send_message_to_room", { room, chat });
     setChat("");
+  };
+  const joinRoom = () => {
+    socket.emit("join_room", room);
   };
 
   return (
     <div className="App">
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          joinRoom();
+        }}
+      >
+        <input
+          type="number"
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) => {
+            setRoom(parseInt(e.target.value));
+          }}
+        />
+        <button>join room</button>
+      </form>
       <h1>Hello, Socket.io!</h1>
-      <input
-        value={chat}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
-          setChat(e.target.value)
-        }
-      />
-      <button onClick={() => sendMessage()}>전송</button>
+      <div>
+        {receiveChat.map((chat) => (
+          <p>{chat}</p>
+        ))}
+      </div>
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          sendMessage();
+        }}
+      >
+        <input
+          value={chat}
+          onChange={(e: React.ChangeEvent<HTMLInputElement>) =>
+            setChat(e.target.value)
+          }
+        />
+
+        <button>전송</button>
+      </form>
     </div>
   );
 }
--- src/service/server.ts
+++ src/service/server.ts
@@ -1,12 +1,6 @@
 import { io } from "socket.io-client";
-export const socket = io("http://localhost:8080");
 
-socket.on("connection", (socket) => {
-  socket.on("connect", () => {
-    console.log(socket.id); // x8WIv7-mJelg7on_ALbx
-  });
+// "undefined" means the URL will be computed from the `window.location` object
+const URL = "http://localhost:8080";
 
-  socket.on("disconnect", () => {
-    console.log(socket.id); // undefined
-  });
-});
+export const socket = io(URL);

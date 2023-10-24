1f7ba83634f1da34714c34ffd4053c742d89f3ec
Hwirin-Kim <kimhuirin@gmail.com>
2023-10-24 19:38:50 +0900

    feat : login컴포넌트 작성완료 (와이어프레임 단계)

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,12 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import Login from "../components/Login";
 import SignUp from "../components/SignUp";
 
 export default function HomePage() {
   const navigation = useNavigate();
   return (
     <>
-      <SignUp />
+      <Login />
       <h1>오델로 페이지</h1>
       <button onClick={() => navigation("/lobby")}>로비로 이동하기</button>
     </>
